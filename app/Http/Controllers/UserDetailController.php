babf19c408a574a9a39be53828cad3bd6dc4c6d2
daekyu.park <mamma1234@gmail.com>
2022-03-10 20:47:06 +0900

    update

--- app/Http/Controllers/UserDetailController.php
+++ app/Http/Controllers/UserDetailController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\UserDetail;
+
 class UserDetailController extends Controller
 {
     public function show($id)
     {
         // dd($id);
 
-        $user = User::find($id)->userDetail;
-        dd($user);
+        // $user = User::find($id)->userDetail;
+        // dd($user);
+
+        $user_detail = UserDetail::find($id);
+        dd($user_detail);
     }
 }
--- app/UserDetail.php
+++ app/UserDetail.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserDetail extends Model
 {
-    //
+    // public function user()
+    // {
+    //     return $this->belongsTo('App\User');
+    // }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'foreign_key', 'other_key');
+    }
 }
